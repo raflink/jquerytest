1ea055ea8792f7e7ef8c56459254597278dab70b
Raffi <rmanisalian@cinemoz.com>
2013-07-02 13:28:38 +0300

    ported to jade

--- js/first.js
+++ js/first.js
@@ -7,10 +7,14 @@ server.configure(function() {
 	server.set('port', process.env.PORT || 8000);
 	server.use(express.bodyParser());
 	server.use(express.methodOverride());
-	server.use(express.static('public')); // kani folderin anoune public e extjs-in
+	server.set('view engine', 'jade');
+	server.use(express.static('public'));
 	server.enable('trust proxy');
 });
 
+server.get('/', function(req, res){
+	res.render('index');
+});
 server.post('/form/redirection', form.redirection);
 
 http.createServer(server).listen(server.get('port'));
